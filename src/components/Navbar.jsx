8ac4310e892377084a6289b950e338411fd7a335
hassaammgl <h455a4mmehtab@gmail.com>
2025-04-19 22:02:10 +0500

    feat: add react-scroll dependency and implement GSAP animation in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { FaBars, } from "react-icons/fa";
 import { IoCloseSharp } from "react-icons/io5";
+import gsap from 'gsap';
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
+    const navRef = useRef(null);
+    const tl = gsap.timeline({ paused: true });
+    tl.to(navRef.current, {
+        duration: 0.5,
+        opacity: 1,
+        y: 0,
+        clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+        ease: "power2.out"
+    })
     const handleToggle = () => {
         setOpen(!open);
+        if (open) {
+            tl.reverse();
+        } else {
+            tl.play();
+        }
     }
     return (
         <>
             <nav className='group w-full h-16 bg-primary transition-all duration-500 ease-in-out hover:bg-accent flex items-center justify-between px-4 text-white'>
-                <h1 className='group-hover:text-black font-extrabold font-'>HSM<span className='group-hover:text-white
-                text-accent'>.</span></h1>
+                <h1 className='group-hover:text-black font-extrabold font-'>HSM<span className='text-accent group-hover:text-white
+                '>.</span></h1>
                 <button onClick={handleToggle} className='text-white group-hover:text-black font-bold'>
                     {open ? <IoCloseSharp className=' text-2xl' /> : <FaBars />}
                 </button>
