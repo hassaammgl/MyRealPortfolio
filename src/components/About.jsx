48f114d22b2c0640e91824d5df469cb327be7fb7
hassaammgl <h455a4mmehtab@gmail.com>
2025-04-27 00:29:16 +0500

    feat: Update About component layout and add new images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,24 @@ import { AnimatedMarquee } from '@/utils/About'
 const About = () => {
   return (
     <Element name="About">
-      <section className="relative h-dvh w-screen ">
+      <section className="relative h-fit w-screen ">
         <AnimatedMarquee />
-
+        <div className='h-dvh w-full relative flex items-center justify-between bg-secondary'>
+          <div className='h-full w-1/2 flex flex-col items-center justify-center p-10'>
+            <h1 className='text-4xl font-bold text-primary'>About Me</h1>
+            <p className='text-lg text-white mt-5'>
+              I am a passionate web developer with a knack for creating dynamic and responsive web applications. My journey in the tech world has been fueled by a love for problem-solving and a desire to learn continuously.
+            </p>
+            <button>
+              Contact Us
+            </button>
+          </div>
+          <div className='h-full w-1/2 relative'>
+            <img src="/about.png" alt="me" className='w-full h-full object-cover' />
+            <img className='absolute' src="/code.png" alt="img-codes" />
+            <img className='absolute' src="/code-2.png" alt="img-codes" />
+          </div>
+        </div>
       </section>
     </Element>
   )
@@ -15,3 +30,4 @@ const About = () => {
 
 
 export default About
+
