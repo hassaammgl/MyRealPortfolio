e8f2ec7cf9ac78f2914847b78e6fec4d26eaba16
hassaammgl <h455a4mmehtab@gmail.com>
2025-04-27 15:56:44 +0500

    feat: Adjust About component layout for improved responsiveness and text scaling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,9 +8,9 @@ const About = () => {
       <section className="relative h-fit w-screen ">
         <AnimatedMarquee />
 
-        <div className="flex flex-col md:flex-row bg-primary text-white h-screen w-full">
+        <div className="flex flex-col md:flex-row bg-primary text-white sm:h-fit w-full">
           <div className="w-full md:w-1/2 p-6 flex items-center">
-            <h1 className=" text-[7vw]  font-bold leading-tight uppercase">
+            <h1 className="text-[7vw] md:text-3xl  font-bold leading-tight uppercase">
               <AnimatedText wordType='f' hover={"hover:text-accent"} text={"I'm a full-stack developer who thrives on turning complex challenges into seamless web experiences. From React to Node.js, I craft scalable apps with precision, always learning and adapting to stay ahead of the curve."} />
             </h1>
           </div>
