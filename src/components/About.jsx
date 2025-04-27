82cd567dca481d4681d7f1afeee5614b78ff7690
hassaammgl <h455a4mmehtab@gmail.com>
2025-04-27 17:41:21 +0500

    feat: Update About component layout for better responsiveness and add new image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,8 +10,8 @@ const About = () => {
 
         <div className="flex flex-col md:flex-row bg-primary text-white sm:h-fit w-full">
           <div className="w-full md:w-1/2 p-6 flex items-center">
-            <h1 className="text-[7vw] md:text-3xl  font-bold leading-tight uppercase">
-              <AnimatedText wordType='f' hover={"hover:text-accent"} text={"I'm a full-stack developer who thrives on turning complex challenges into seamless web experiences. From React to Node.js, I craft scalable apps with precision, always learning and adapting to stay ahead of the curve."} />
+            <h1 className="text-[6vw] sm:text-3xl  font-bold leading-tight uppercase">
+              <AnimatedText wordType='f' hover={"hover:text-accent transition-all ease-in-out duration-500"} text={"I'm a full-stack developer who thrives on turning complex challenges into seamless web experiences. From React to Node.js, I craft scalable apps with precision, always learning and adapting to stay ahead of the curve."} />
             </h1>
           </div>
 
@@ -19,12 +19,19 @@ const About = () => {
             <img
               src="/about.png"
               alt="Portrait photography scene"
-              className="h-full w-full object-cover"
+              className="h-screen w-full object-cover"
             />
 
             <div className="absolute bottom-10 right-10 w-52 h-36  bg-opacity-50 rounded">
               <img
                 src="/code.png"
+                alt="code motivation"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="absolute top-10 left-10 w-52 h-36  bg-opacity-50 rounded">
+              <img
+                src="/code-2.png"
                 alt="code motivation"
                 className="w-full h-full object-cover"
               />
--- src/components/AnimatedText.jsx
+++ src/components/AnimatedText.jsx
@@ -1,46 +1,7 @@
 import React, { useRef } from 'react'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-// gsap.registerPlugin(ScrollTrigger)
-
-// const AnimatedText = ({ text, containerClass = "", style, hover, wordType = "s" }) => {
-//     const containerRef = useRef(null)
-
-//     useGSAP(() => {
-//         const chars = containerRef.current.querySelectorAll(".char")
-
-//         gsap.from(chars, {
-//             opacity: 0,
-//             y: 70,
-//             duration: 1,
-//             ease: "power3.out",
-//             stagger: {
-//                 each: 0.05,
-//                 from: "start"
-//             },
-//             scrollTrigger: {
-//                 trigger: containerRef.current,
-//                 start: "top 90%",
-//                 end: "bottom 60%",
-//                 toggleActions: "play none none reverse"
-//             }
-//         })
-//     }, { scope: containerRef })
-
-//     return (
-//         <span style={style} ref={containerRef} className={`animated-text ${containerClass}`}>
-//             {text.split(wordType === "s" ? "" : " ").map((char, index) => (
-//                 <span key={index} className={`char inline-block ${hover}`}>
-//                     {char === ' ' ? '\u00A0' : char}
-//                 </span>
-//             ))}
-//         </span>
-//     )
-// }
-
-// export default AnimatedText
 
 const AnimatedText = ({ text, containerClass = "", style, hover, wordType = "s" }) => {
     const containerRef = useRef(null)
