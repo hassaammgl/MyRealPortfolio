3c5bc2e61d1a74799f82f03604e55f9e6dec377c
hassaammgl <h455a4mmehtab@gmail.com>
2025-05-04 15:19:29 +0500

    feat: Add class to AnimatedText component in Footer for improved styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="bg-primary text-white">
             <h1 className="text-[15vw] font-ruslan text-center font-light mt-10 uppercase leading-48">
-                <AnimatedText text={"HASSAAM"} />
+                <AnimatedText className='text-tertiary' text={"HASSAAM"} />
             </h1>
             <div className="bg-accent text-black text-sm py-4 px-6 flex flex-col md:flex-row justify-between items-center gap-4">
                 <p>Copyright © hassaammgl {new Date().getFullYear()}</p>
