4b40640abff940983e325544c8f77f7c5cfa9c43
hassaammgl <h455a4mmehtab@gmail.com>
2025-05-18 23:33:09 +0500

    feat: Update Hero component with new layout, animations, and content enhancements

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import React, { Suspense, lazy } from 'react'
-import Loader from "@/components/Loader";
+import React from 'react'
 
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer';
-import ResponsiveTester from '@/components/ResponsiveTester';
+// import ResponsiveTester from '@/components/ResponsiveTester';
 import Wrapper from '@/components/Wrapper';
 import AnimatedCursor from '@/utils/Cursor';
 // import GitHubStats from './components/GithubStats';
 
-const Hero = lazy(() => import("@/components/Hero"))
-const About = lazy(() => import("@/components/About"))
-const Services = lazy(() => import("@/components/Services"))
-const Projects = lazy(() => import("@/components/Projects"))
-const Contact = lazy(() => import("@/components/Contact"))
 import CanvasParticles from './components/Particles';
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Services from "@/components/Services";
+import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
 
 const App = () => {
   return (
@@ -21,15 +20,13 @@ const App = () => {
       <Navbar />
       <AnimatedCursor />
       <CanvasParticles />
-      <Suspense fallback={<Loader />} >
-        <Hero />
-        <About />
-        <Services />
-        <Projects />
-        {/* <ResponsiveTester /> */}
-        <Contact />
-        <Footer />
-      </Suspense>
+      <Hero />
+      <About />
+      <Services />
+      <Projects />
+      {/* <ResponsiveTester /> */}
+      <Contact />
+      <Footer />
     </Wrapper>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,8 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import AnimatedText from '@/components/AnimatedText';
 import Load from '@/components/Load';
+import { useGSAP } from '@gsap/react';
+import { FiArrowDownRight } from 'react-icons/fi';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -15,30 +17,54 @@ const Hero = () => {
         <Element name="Home">
             <section
                 ref={sectionRef}
-                className="relative h-dvh w-screen flex justify-center items-center bg-center bg-cover overflow-hidden"
+                className="relative h-screen w-screen flex justify-center items-center bg-center bg-cover overflow-hidden"
             >
                 <Load setOnLoadComplete={setOnLoadComplete} />
                 {
                     onLoadComplete && (
-                        <div className='flex flex-col items-center justify-center w-full'>
-                            <h1 className="text-[13vw] text-white font-boldonse overflow-hidden pb-4 md:pb-8">
+                        <div className='flex flex-col items-stretch justify-center w-full'>
+                            <h1
+                                className="text-[13vw] h-[60vh] flex justify-center items-center text-white font-boldonse overflow-hidden pb-4 md:pb-8 z-10"
+                            >
                                 <AnimatedText
                                     text={"Hassaammgl"}
                                     className="text-center"
                                     hoverClass="hover:text-accent transition-colors duration-500 ease-in-out"
                                 />
                             </h1>
-                            <div className="relative max-w-7xl flex flex-col items-center justify-start w-full h-[40vh] md:h-[50vh] rounded-xl md:rounded-2xl">
-                                <video
-                                    className="w-full h-full object-cover absolute top-0 left-0"
-                                    autoPlay
-                                    loop
-                                    muted
-                                    playsInline
-                                    src="/hero/hero.mp4"
-                                />
-                                <div className="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-black to-transparent"></div>
-                                <img src="/hero/hero.png" className="absolute z-10 -top-32 right-6 rounded-2xl" alt="" />
+                            <div className="flex items-start justify-between relative">
+                                <div className="w-1/3 px-10">
+                                    <FiArrowDownRight className='text-white text-5xl' />
+                                    <p className="text-xl md:text-2xl mb-8 font-Audiowide leading-relaxed text-stone-200">
+                                        I help growing brands and startups
+                                        <br />gain an unfair advantage through
+                                        <br />premium, results driven websites.
+                                    </p>
+
+                                    <button
+                                        className="inline-flex items-center px-8 py-4 bg-stone-800 text-white rounded-full hover:bg-stone-700 transition-colors"
+                                    >
+                                        BOOK A CALL
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                                        </svg>
+                                    </button>
+                                </div>
+
+                                <div className="w-1/3">
+                                    <img
+                                        src="/hero/hero.png"
+                                        alt="Portrait photo of hassaam"
+                                        className="absolute -top-72 object-cover"
+                                    />
+                                </div>
+
+                                <div className="w-1/3 text-right px-20 h-full ">
+                                    <div className='text-white mt-auto'>
+                                        <p className="text-stone-600 text-sm mb-1">AVAILABLE FOR WORK</p>
+                                        <h3 className="text-5xl md:text-6xl font-bold tracking-tighter">{Months[new Date().getMonth()]} '{new Date().getDate()}</h3>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     )
@@ -49,3 +75,18 @@ const Hero = () => {
 };
 
 export default Hero;
+
+const Months = {
+    0: "Jan",
+    1: "Feb",
+    2: "Mar",
+    3: "Apr",
+    4: "May",
+    5: "Jun",
+    6: "Jul",
+    7: "Aug",
+    8: "Sep",
+    9: "Oct",
+    10: "Nov",
+    11: "Dec",
+}
