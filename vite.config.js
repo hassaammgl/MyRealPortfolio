7a10e42cbcc6451eb23fccb521953a1232c07f16
hassaammgl <h455a4mmehtab@gmail.com>
2025-05-20 23:50:38 +0500

    refactor: remove unused dependencies and related plugin from configuration

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,11 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath } from 'url';
 import path from 'path';
-import glsl from 'vite-plugin-glsl'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), glsl()],
+  plugins: [react(), tailwindcss(),],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
