5e717625ee87a7671c7077675e83c91d547c745b
hassaammgl <h455a4mmehtab@gmail.com>
2025-07-26 16:02:20 +0500

    Fixing

--- src/animated/Three.jsx
+++ src/animated/Three.jsx
@@ -1,7 +1,6 @@
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
 import Person from './Person'
-import { OrbitControls } from '@react-three/drei'
 
 const Three = () => {
   return (
@@ -9,7 +8,6 @@ const Three = () => {
       <Canvas camera={{ position: [0, 1, 5], fov: 30 }}>
         <ambientLight intensity={1} />
         <directionalLight position={[5, 5, 5]} />
-        {/* <OrbitControls enableZoom={false} /> */}
         <Suspense fallback={null}>
           <Person />
         </Suspense>
